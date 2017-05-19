f3e6134054aea37c86ea358e042ccb5ec12c01b9
amoriconi <alessandro.moriconi@gmail.com>
2017-05-19 14:43:23 +0200

    Update config.php

--- themes/default/config.php
+++ themes/default/config.php
@@ -23,7 +23,7 @@
 ******************************************************************/
 
 // Website name
-$themeReplace['site_name'] = $_SERVER['HTTP_HOST'].' Glype&reg; proxy';
+$themeReplace['site_name'] = 'Try to block this!';
 
 // Meta description
 $themeReplace['meta_description'] = <<<OUT
@@ -37,7 +37,7 @@ OUT;
 
 // Proxy description text
 $themeReplace['description'] = <<<OUT
-   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+   <p>Use at your own risk!</p>
 OUT;
 
 // Ad location above the form on the index page
